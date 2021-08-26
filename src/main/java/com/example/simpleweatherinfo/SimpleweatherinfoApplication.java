8c56a7ce0c4855f02ac501ae7833193fe4f703f9
Tohanos <kravchenko_ae@mail.ru>
2021-08-26 09:16:07 +0300

    Initial commit

---
+++ src/main/java/com/example/simpleweatherinfo/SimpleweatherinfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleweatherinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleweatherinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleweatherinfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpleweatherinfo/SimpleweatherinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleweatherinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleweatherinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
